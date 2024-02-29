e79f5ec6a44c00d0510248cc2dfe976aa65f2895
Iryna Burlaka <i.am.blinova@gmail.com>
2024-02-29 11:06:22 +0100

    typing of routes, svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import { Suspense, lazy, LazyExoticComponent } from "react";
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 
-// import React from "react";
-// type LazyComponentType = LazyExoticComponent<() => JSX.Element>;
-
 const Home = lazy(() => import("./pages/HomePage"));
-// const Catalog = lazy(() => import("pages/CatalogPage"));
-// const Favorites = lazy(() => import("pages/FavoritesPage"));
+const Catalog = lazy(() => import("./pages/CatalogPage"));
+const Favorites = lazy(() => import("./pages/FavoritesPage"));
 
 function App() {
   return (
-    // <Suspense>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        {/* <Route path="/catalog" element={<Catalog />} />
-           <Route path="/favorites" element={<Favorites />} />
-           <Route path="*" element={<Navigate to="/" />} />*/}
-      </Route>
-    </Routes>
-    // </Suspense>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import { Circles } from "react-loader-spinner";
 import { Loading } from "./Loader.styled";
 
-export const Loader = () => {
+export const Loader: React.FC = () => {
   return (
     <Loading>
       <Circles height="80" width="80" color="#407bff" ariaLabel="circles-loading" wrapperStyle={{}} wrapperClass="" visible={true} />
